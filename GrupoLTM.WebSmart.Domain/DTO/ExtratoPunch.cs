7bbbcf3566c277d91e08043a484b6f9a96072cbc
hudson3x3 <164959106+hudson3x3@users.noreply.github.com>
2024-05-28 13:17:16 -0300

    Add files via upload

---
+++ GrupoLTM.WebSmart.Domain/DTO/ExtratoPunch.cs
@@ -0,0 +1,33 @@
+﻿using GrupoLTM.WebSmart.Domain.Enums;
+
+namespace GrupoLTM.WebSmart.Domain.DTO
+{
+    public class ExtratoPunch
+    {
+        public string IncentiveProgram { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public string StatusPoints { get; set; }
+
+        public decimal Points { get; set; }
+
+        public long? AvailablePoints { get; set; }
+
+        public long? PendingPoints { get; set; }
+
+        public long? CanceledPoints { get; set; }
+
+        public EnumDomain.TipoArquivo TipoArquivo { get; set; }
+
+        public int CampaignNumber { get; set; }
+
+        public int CampaignYear { get; set; }
+
+        public int? EndCampaignNumber { get; set; }
+
+        public int? EndCampaignYear { get; set; }
+
+        public int OrderId { get; set; }
+    }
+}
---
+++ GrupoLTM.WebSmart.Domain/DTO/LogAccountDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GrupoLTM.WebSmart.Domain.DTO
+{
+    public class LogAccountDTO
+    {
+        public int Id { get; set; }
+        public string LoginAdmin { get; set; }
+        public string AccountNumber { get; set; }
+        public string NameAccountNumber { get; set; }
+        public string IP { get; set; }
+        public DateTime DataInclusao { get; set; }
+        public int CatalogoId { get; set; }
+        public string Catalogo { get; set; }
+    }
+}
